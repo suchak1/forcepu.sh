8249a04cfe8e1ffdb2d1885e0d34f3ac1a3cd2a5
Krish Suchak <suchak.krish@gmail.com>
2021-06-28 02:32:01 -0400

    config file

---
+++ config/config.ts
@@ -0,0 +1,16 @@
+export default {
+  // presets: [
+  //   '@umijs/preset-react'
+  // ],
+  plugins: [],
+  antd: { compact: true, dark: true },
+  dva: {
+    // immer: true,
+    hmr: true,
+  },
+  locale: {
+    // default: 'us-EN',
+    baseNavigator: true,
+  },
+  theme: {}
+}
