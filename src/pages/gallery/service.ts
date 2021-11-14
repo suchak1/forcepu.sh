4a4705f5ba86c4af824e5bda5458ba59b5bdd5a6
Krish Suchak <suchak.krish@gmail.com>
2021-11-14 03:00:22 -0500

    add gallery files

---
+++ src/pages/gallery/service.ts
@@ -0,0 +1,9 @@
+// import { request } from 'umi';
+
+export class GalleryService {
+  // async fetch() {
+  //   await fetch('/api/exercise_log', { method: 'GET' })
+  //   .then(response => response.json())
+  //   .then(data => data);
+  // }
+}
