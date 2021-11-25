b844ce59000d6cec3f77308590d84e60cf00268d
Krish Suchak <suchak.krish@gmail.com>
2021-11-25 03:12:46 -0500

    just holding

--- src/api/backend/home.py
+++ src/api/backend/home.py
@@ -15,26 +15,26 @@ def get_holding(*_):
     reader = md.reader
     finder = md.finder
     store = reader.store
-    store.download_file(finder.get_signals_path(), '/tmp/')
-    signals_filename = f'/tmp/{finder.get_signals_path()}'
-    signals_df = pd.read_csv(signals_filename)
+    # store.download_file(finder.get_signals_path(), '/tmp/')
+    # signals_filename = f'/tmp/{finder.get_signals_path()}'
+    # signals_df = pd.read_csv(signals_filename)
 
     symbol = C.POLY_CRYPTO_DICT['BTC']
     store.download_file(finder.get_ohlc_path(symbol, 'polygon'), '/tmp/')
     btc_filename = f'/tmp/{finder.get_ohlc_path(symbol, "polygon")}'
     btc_df = pd.read_csv(btc_filename)
 
-    df = btc_df.merge(signals_df, on=C.TIME)[C.CLOSE, C.SIG]
-    close = df[C.CLOSE].to_numpy()
-    signals = df[C.SIG].to_numpy()
+    # df = btc_df.merge(signals_df, on=C.TIME)[C.CLOSE, C.SIG]
+    close = btc_df[C.CLOSE].to_numpy()
+    # signals = df[C.SIG].to_numpy()
     hist = Historian()
     holding = hist.buy_and_hold(close)
     holding_balances = list(holding.value())
     holding_stats = dict(holding.stats())
 
-    hyper = hist.create_portfolio(close, signals, 0.001)
-    hyper_balances = list(hyper.value())
-    hyper_stats = dict(hyper.stats())
+    # hyper = hist.create_portfolio(close, signals, 0.001)
+    # hyper_balances = list(hyper.value())
+    # hyper_stats = dict(hyper.stats())
 
     # df = md.get_ohlc(symbol='TSLA', timeframe='5d')
     # reader = FileReader()
@@ -51,7 +51,7 @@ def get_holding(*_):
         "statusCode": 200,
         "body": {
             'holding': {'balances': holding_balances, 'stats': holding_stats},
-            'hyper': {'balances': hyper_balances, 'stats': hyper_stats},
+            # 'hyper': {'balances': hyper_balances, 'stats': hyper_stats},
         },
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
