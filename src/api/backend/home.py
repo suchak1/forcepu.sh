5efbe22cff418a36ebd7fc6b64b28d79f21f970c
Krish Suchak <suchak.krish@gmail.com>
2021-11-25 03:00:01 -0500

    times out

--- src/api/backend/home.py
+++ src/api/backend/home.py
@@ -1,7 +1,9 @@
 import os
-from hyperdrive import DataSource, FileOps
+import pandas as pd
+from hyperdrive import DataSource, FileOps, History, Constants as C
 from FileOps import FileReader
 from DataSource import IEXCloud, MarketData
+from History import Historian
 
 
 def get_holding(*_):
@@ -10,19 +12,47 @@ def get_holding(*_):
     #     print(pf.value())
     # print(os.environ)
     md = MarketData()
-    df = md.get_ohlc(symbol='TSLA', timeframe='5d')
+    reader = md.reader
+    finder = md.finder
+    store = reader.store
+    store.download_file(finder.get_signals_path(), '/tmp/')
+    signals_filename = f'/tmp/{finder.get_signals_path()}'
+    signals_df = pd.read_csv(signals_filename)
+
+    symbol = C.POLY_CRYPTO_DICT['BTC']
+    store.download_file(finder.get_ohlc_path(symbol, 'polygon'), '/tmp/')
+    btc_filename = f'/tmp/{finder.get_ohlc_path(symbol, "polygon")}'
+    btc_df = pd.read_csv(btc_filename)
+
+    df = btc_df.merge(signals_df, on=C.TIME)[C.CLOSE, C.SIG]
+    close = df[C.CLOSE].to_numpy()
+    signals = df[C.SIG].to_numpy()
+    hist = Historian()
+    holding = hist.buy_and_hold(close)
+    holding_balances = list(holding.value())
+    holding_stats = dict(holding.stats())
+
+    hyper = hist.create_portfolio(close, signals, 0.001)
+    hyper_balances = list(hyper.value())
+    hyper_stats = dict(hyper.stats())
+
+    # df = md.get_ohlc(symbol='TSLA', timeframe='5d')
     # reader = FileReader()
     # df = reader.load_csv('models/latest/signals.csv')
 
-    print(df)
+    # print(df)
+    # s3 = boto3.resource('s3')
+    # bucket = s3.Bucket('hyperdrive.pro')
+    # with open('/tmp/sig.csv', 'wb') as file:
+    #     bucket.download_fileobj(md.finder.get_signals_path(), file)
+    # df = pd.read_csv('/tmp/sig.csv')
+    # print(df)
     return {
         "statusCode": 200,
         "body": {
-            'bucket': md.reader.store.bucket_name,
-            'df': df.to_dict(),
-            'env': os.environ
+            'holding': {'balances': holding_balances, 'stats': holding_stats},
+            'hyper': {'balances': hyper_balances, 'stats': hyper_stats},
         },
-        # "body": {'environ': dict(os.environ)},
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
 
