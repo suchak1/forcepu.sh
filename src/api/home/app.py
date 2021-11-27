ab1580412bd173bd627f0337fa22529d6e094d2d
Krish Suchak <suchak.krish@gmail.com>
2021-11-26 23:09:53 -0500

    os.environ

--- src/api/home/app.py
+++ src/api/home/app.py
@@ -7,7 +7,7 @@ s3 = boto3.client('s3')
 
 def get_hyper(*_):
     obj = s3.get_object(
-        Bucket=os.environment['S3_BUCKET'], Key='data/api/hyper.json')
+        Bucket=os.environ['S3_BUCKET'], Key='data/api/hyper.json')
     res = obj['Body'].read()
     data = json.loads(res)
     return {
@@ -19,7 +19,7 @@ def get_hyper(*_):
 
 def get_holding(*_):
     obj = s3.get_object(
-        Bucket=os.environment['S3_BUCKET'], Key='data/api/holding.json')
+        Bucket=os.environ['S3_BUCKET'], Key='data/api/holding.json')
     res = obj['Body'].read()
     data = json.loads(res)
     return {
