3e28ef6fff17246a382039f0dc4b1436242ff74d
Krish Suchak <suchak.krish@gmail.com>
2022-03-12 21:49:14 -0500

    try if/else

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -24,44 +24,47 @@ const redirectUrl = isDev
   ? `https://${devHostname}`
   : `https://${prodHostname}`;
 
-const configuration = isLocal
-  ? import("@/aws-exports")
-  : {
-      aws_project_region: process.env.REACT_APP_REGION,
-      aws_cognito_identity_pool_id: process.env.REACT_APP_IDENTITY_POOL_ID,
-      aws_cognito_region: process.env.REACT_APP_REGION,
-      aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
-      aws_user_pools_web_client_id: process.env.REACT_APP_WEB_CLIENT_ID,
-      oauth: {
-        domain: process.env.REACT_APP_OAUTH_DOMAIN,
-        scope: [
-          "phone",
-          "email",
-          "openid",
-          "profile",
-          "aws.cognito.signin.user.admin",
-        ],
-        redirectSignIn: redirectUrl,
-        redirectSignOut: redirectUrl,
-        responseType: "code",
-      },
-      federationTarget: "COGNITO_USER_POOLS",
-      aws_cognito_username_attributes: ["EMAIL"],
-      aws_cognito_social_providers: ["GOOGLE"],
-      aws_cognito_signup_attributes: ["EMAIL", "NAME", "PICTURE"],
-      aws_cognito_mfa_configuration: "OPTIONAL",
-      aws_cognito_mfa_types: ["TOTP"],
-      aws_cognito_password_protection_settings: {
-        passwordPolicyMinLength: 8,
-        passwordPolicyCharacters: [
-          "REQUIRES_LOWERCASE",
-          "REQUIRES_NUMBERS",
-          "REQUIRES_SYMBOLS",
-          "REQUIRES_UPPERCASE",
-        ],
-      },
-      aws_cognito_verification_mechanisms: ["EMAIL"],
-    };
+let configuration;
+if (isLocal) {
+  configuration = import("@/aws-exports");
+} else {
+  configuration = {
+    aws_project_region: process.env.REACT_APP_REGION,
+    aws_cognito_identity_pool_id: process.env.REACT_APP_IDENTITY_POOL_ID,
+    aws_cognito_region: process.env.REACT_APP_REGION,
+    aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
+    aws_user_pools_web_client_id: process.env.REACT_APP_WEB_CLIENT_ID,
+    oauth: {
+      domain: process.env.REACT_APP_OAUTH_DOMAIN,
+      scope: [
+        "phone",
+        "email",
+        "openid",
+        "profile",
+        "aws.cognito.signin.user.admin",
+      ],
+      redirectSignIn: redirectUrl,
+      redirectSignOut: redirectUrl,
+      responseType: "code",
+    },
+    federationTarget: "COGNITO_USER_POOLS",
+    aws_cognito_username_attributes: ["EMAIL"],
+    aws_cognito_social_providers: ["GOOGLE"],
+    aws_cognito_signup_attributes: ["EMAIL", "NAME", "PICTURE"],
+    aws_cognito_mfa_configuration: "OPTIONAL",
+    aws_cognito_mfa_types: ["TOTP"],
+    aws_cognito_password_protection_settings: {
+      passwordPolicyMinLength: 8,
+      passwordPolicyCharacters: [
+        "REQUIRES_LOWERCASE",
+        "REQUIRES_NUMBERS",
+        "REQUIRES_SYMBOLS",
+        "REQUIRES_UPPERCASE",
+      ],
+    },
+    aws_cognito_verification_mechanisms: ["EMAIL"],
+  };
+}
 
 Amplify.configure(configuration);
 
