3ace9681208d695f6785e0b8dfca2515b306299e
Krish Suchak <suchak.krish@gmail.com>
2022-03-13 05:25:13 -0400

    move to auth api fx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -9,6 +9,7 @@ import {
   defaultTheme,
 } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import { getApiUrl } from "@/utils";
 import "@aws-amplify/ui-react/styles.css";
 import BTC_ICE from "../../assets/btc_ice.png";
 import overrides from "./index.less";
@@ -24,49 +25,18 @@ const redirectUrl = isDev
   ? `https://${devHostname}`
   : `https://${prodHostname}`;
 
-let configuration;
+let config;
 if (isLocal) {
-  configuration = require("@/aws-exports").default;
+  config = require("@/aws-exports").default;
+  Amplify.configure(config);
 } else {
-  configuration = {
-    aws_project_region: process.env.REACT_APP_REGION,
-    aws_cognito_identity_pool_id: process.env.REACT_APP_IDENTITY_POOL_ID,
-    aws_cognito_region: process.env.REACT_APP_REGION,
-    aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
-    aws_user_pools_web_client_id: process.env.REACT_APP_WEB_CLIENT_ID,
-    oauth: {
-      domain: process.env.REACT_APP_OAUTH_DOMAIN,
-      scope: [
-        "phone",
-        "email",
-        "openid",
-        "profile",
-        "aws.cognito.signin.user.admin",
-      ],
-      redirectSignIn: redirectUrl,
-      redirectSignOut: redirectUrl,
-      responseType: "code",
-    },
-    federationTarget: "COGNITO_USER_POOLS",
-    aws_cognito_username_attributes: ["EMAIL"],
-    aws_cognito_social_providers: ["GOOGLE"],
-    aws_cognito_signup_attributes: ["EMAIL", "NAME", "PICTURE"],
-    aws_cognito_mfa_configuration: "OPTIONAL",
-    aws_cognito_mfa_types: ["TOTP"],
-    aws_cognito_password_protection_settings: {
-      passwordPolicyMinLength: 8,
-      passwordPolicyCharacters: [
-        "REQUIRES_LOWERCASE",
-        "REQUIRES_NUMBERS",
-        "REQUIRES_SYMBOLS",
-        "REQUIRES_UPPERCASE",
-      ],
-    },
-    aws_cognito_verification_mechanisms: ["EMAIL"],
-  };
+  const url = `${getApiUrl}/auth`;
+  const getConfig = async () =>
+    fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .then((config) => Amplify.configure(config));
+  getConfig();
 }
-
-Amplify.configure(configuration);
 
 const theme = createTheme({
   name: "dark-mode-theme",
