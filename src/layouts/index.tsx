b0488f0fa30c1eeaddb798437ce41910e18c736b
Krish Suchak <suchak.krish@gmail.com>
2022-03-13 17:09:13 -0400

    replicate prob

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -23,55 +23,57 @@ if (isLocal) {
   Amplify.configure(config);
 } else {
   // const url = `${getApiUrl()}/auth`;
-  // const url = `https://api.dev.forcepu.sh/auth`;
-  // const getConfig = async () =>
-  //   fetch(url, { method: "GET" })
-  //     .then((response) => response.json())
-  //     .then((config) => {
-  //       console.log(config);
-  //       Amplify.configure(config);
-  //       Amplify.register(Auth);
-  //     });
-  // getConfig();
-  const config = {
-    aws_project_region: "us-east-1",
-    aws_cognito_identity_pool_id:
-      "us-east-1:7ca9aa41-c652-49e9-bca8-ec9f9aa15656",
-    aws_cognito_region: "us-east-1",
-    aws_user_pools_id: "us-east-1_39SPX9fAC",
-    aws_user_pools_web_client_id: "47fs6be1f16egb5eqpvir9fu3u",
-    oauth: {
-      domain: "pzw1msarrwym-dev.auth.us-east-1.amazoncognito.com",
-      scope: [
-        "phone",
-        "email",
-        "openid",
-        "profile",
-        "aws.cognito.signin.user.admin",
-      ],
-      redirectSignIn: "https://dev.forcepu.sh",
-      redirectSignOut: "https://dev.forcepu.sh",
-      responseType: "code",
-    },
-    federationTarget: "COGNITO_USER_POOLS",
-    aws_cognito_username_attributes: ["EMAIL"],
-    aws_cognito_social_providers: ["GOOGLE"],
-    aws_cognito_signup_attributes: ["EMAIL", "NAME", "PICTURE"],
-    aws_cognito_mfa_configuration: "OPTIONAL",
-    aws_cognito_mfa_types: ["TOTP"],
-    aws_cognito_password_protection_settings: {
-      passwordPolicyMinLength: 8,
-      passwordPolicyCharacters: [
-        "REQUIRES_LOWERCASE",
-        "REQUIRES_NUMBERS",
-        "REQUIRES_SYMBOLS",
-        "REQUIRES_UPPERCASE",
-      ],
-    },
-    aws_cognito_verification_mechanisms: ["EMAIL"],
-  };
-  Amplify.configure(config);
-  // Amplify.register(Auth);
+  const url = `https://api.dev.forcepu.sh/auth`;
+  const getConfig = async () =>
+    fetch(url, { method: "GET" })
+      .then((response) => response.json())
+      .then((config) => {
+        // console.log(config);
+        Amplify.configure(config);
+        // Amplify.register(Auth);
+        // return config;
+      });
+  getConfig();
+  // Amplify.configure(getConfig().then((config) => config));
+
+  // const config = {
+  //   aws_project_region: "us-east-1",
+  //   aws_cognito_identity_pool_id:
+  //     "us-east-1:7ca9aa41-c652-49e9-bca8-ec9f9aa15656",
+  //   aws_cognito_region: "us-east-1",
+  //   aws_user_pools_id: "us-east-1_39SPX9fAC",
+  //   aws_user_pools_web_client_id: "47fs6be1f16egb5eqpvir9fu3u",
+  //   oauth: {
+  //     domain: "pzw1msarrwym-dev.auth.us-east-1.amazoncognito.com",
+  //     scope: [
+  //       "phone",
+  //       "email",
+  //       "openid",
+  //       "profile",
+  //       "aws.cognito.signin.user.admin",
+  //     ],
+  //     redirectSignIn: "https://dev.forcepu.sh",
+  //     redirectSignOut: "https://dev.forcepu.sh",
+  //     responseType: "code",
+  //   },
+  //   federationTarget: "COGNITO_USER_POOLS",
+  //   aws_cognito_username_attributes: ["EMAIL"],
+  //   aws_cognito_social_providers: ["GOOGLE"],
+  //   aws_cognito_signup_attributes: ["EMAIL", "NAME", "PICTURE"],
+  //   aws_cognito_mfa_configuration: "OPTIONAL",
+  //   aws_cognito_mfa_types: ["TOTP"],
+  //   aws_cognito_password_protection_settings: {
+  //     passwordPolicyMinLength: 8,
+  //     passwordPolicyCharacters: [
+  //       "REQUIRES_LOWERCASE",
+  //       "REQUIRES_NUMBERS",
+  //       "REQUIRES_SYMBOLS",
+  //       "REQUIRES_UPPERCASE",
+  //     ],
+  //   },
+  //   aws_cognito_verification_mechanisms: ["EMAIL"],
+  // };
+  // Amplify.configure(config);
 }
 
 const theme = createTheme({
