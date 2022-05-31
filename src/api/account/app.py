127aeb7ef847c95506e31cf76f1befea9c5eac63
Krish Suchak <suchak.krish@gmail.com>
2022-05-30 22:49:07 -0400

    add dynamo logic

--- src/api/account/app.py
+++ src/api/account/app.py
@@ -1,4 +1,9 @@
+import os
 import json
+import secrets
+from pynamodb.models import Model
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+from pynamodb.attributes import UnicodeAttribute, MapAttribute, BooleanAttribute
 
 
 def get_account(event, _):
@@ -45,18 +50,62 @@ def handle_get(event):
     verified = verify_user(claims)
 
     status_code = 401
-    body = {'message': 'This account is not verified.'}
+    body = json.dumps({'message': 'This account is not verified.'})
 
     if verified:
         status_code = 200
-        body = claims
+        email = claims['email']
+        try:
+            user = UserModel.get(email)
+        except UserModel.DoesNotExist:
+            user = UserModel(email)
+            user.save()
+        body = user.to_json()
 
     return {
         "statusCode": status_code,
-        "body": json.dumps(body),
+        "body": body,
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
 
-# to generate api key:
-# import secrets
-# secrets.token_urlsafe(64)
+
+class Permissions(MapAttribute):
+    is_admin = BooleanAttribute(default=False)
+    is_friend = BooleanAttribute(default=False)
+
+
+def get_api_key():
+    continue_looping = True
+    while continue_looping:
+        api_key = secrets.token_urlsafe(64)
+        query_results = list(UserModel.api_key_index.query(api_key))
+        continue_looping = len(query_results)
+    return api_key
+
+
+class APIKeyIndex(GlobalSecondaryIndex):
+    """
+    This class represents a global secondary index
+    """
+    class Meta:
+        # index_name is optional, but can be provided to override the default name
+        index_name = 'api_key_index'
+        # All attributes are projected
+        projection = AllProjection()
+
+    # This attribute is the hash key for the index
+    # Note that this attribute must also exist
+    # in the model
+    api_key = UnicodeAttribute(hash_key=True)
+
+
+class UserModel(Model):
+    """
+    A DynamoDB User
+    """
+    class Meta:
+        table_name = os.environ['TABLE_NAME']
+    email = UnicodeAttribute(hash_key=True)
+    api_key = UnicodeAttribute(default=get_api_key)
+    permissions = MapAttribute(attr_name="permissions", default=Permissions)
+    api_key_index = APIKeyIndex()
