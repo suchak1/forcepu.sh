d554f7b5aa5c61a5ca9fc53207d8aef6a9cf9f13
Krish Suchak <suchak.krish@gmail.com>
2022-06-14 15:42:03 -0400

    update scripts

---
+++ util/install_sam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+curl -LO https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+rm -rf sam-installation aws-sam-cli-linux-x86_64.zip
+
+raw_new_v=$(sam --version)
+new_v="${raw_new_v##*n }"
+
+echo "Installed SAM CLI (v${new_v})"
