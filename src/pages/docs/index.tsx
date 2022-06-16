fedb9e0dfc4ae20b82405357f434862bf55cac1b
Krish Suchak <suchak.krish@gmail.com>
2022-06-16 03:45:45 -0400

    add swagger

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Layout, Typography, Table, Input } from "antd";
+import { Layout, Typography, Table, Input, message, Button } from "antd";
 import { getApiUrl, useAccount } from "@/utils";
 import swaggerSpec from "../../api/spec/swagger.json";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { CopyOutlined } from "@ant-design/icons";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import styles from "./index.less";
@@ -37,21 +38,17 @@ const DocsPage = () => {
 
   useEffect(useAccount(loggedIn, setAccount, setAccountLoading), [loggedIn]);
 
-  //   useEffect(() => {
-  //     (async () => {
-  //       const url = "https://s3.amazonaws.com/api.forcepu.sh/resume.pdf";
-  //       fetch(url, { method: "GET" })
-  //         .then((response) => response.json())
-  //         .then((data) => setLog(data))
-  //         .then(() => setLoading(false));
-  //     })();
-  //   }, []);
+  const copyToClipboard = (val: string, name: string) =>
+    navigator.clipboard.writeText(val).then(
+      () => message.success(`Copied ${name} to clipboard.`),
+      () => message.error(`Did not copy ${name} to clipboard`)
+    );
 
   return (
     // <Layout>
     // <Layout.Content style={{ padding: 24 }}>
     <>
-      <Title>Resume</Title>
+      <Title>API Docs</Title>
       {/* add message when req fails that you need a valid api key */}
       {/* login and fetch account */}
       {/* hide api key component or indicate that you can login to get one */}
@@ -65,16 +62,15 @@ const DocsPage = () => {
             readOnly
           />
           {/* change input focus color to cyan */}
-          {/* <Button */}
-          {/* onClick={() => */}
-          {/* copyToClipboard(account?.api_key, "API Key") */}
-          {/* } */}
-          {/* icon={<CopyOutlined />} */}
-          {/* /> */}
+          <Button
+            onClick={() => copyToClipboard(account?.api_key, "API Key")}
+            icon={<CopyOutlined />}
+          />
           {/* handle copying to clipboard */}
           {/* show success or info alert for a few sec at top when Copy button is pressed */}
         </span>
       </Input.Group>
+      <SwaggerUI spec={swaggerSpec} />
     </>
     // </Layout.Content>
     // </Layout>
@@ -94,16 +90,8 @@ export default DocsPage;
 //   background: #52e5ff;
 // }
 
-// const copyToClipboard = (val: string, name: string) =>
-//   navigator.clipboard.writeText(val).then(
-//     () => message.success(`Copied ${name} to clipboard.`),
-//     () => message.error(`Did not copy ${name} to clipboard`)
-//   );
-
-// {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" /> */}
 // {/* <SwaggerUI spec={swaggerSpec} /> */}
 // {/* use signals_ui.pdf in downloads folder as guide, keep wide screen - better for mobile */}
 // {/* remove params wrapper div, remove info div, rename algo to Signals API or Algo API, ,  */}
 // {/* requestInterceptor => should inject api key if necessary, responseInterceptor => should reveal cards on left */}
-// {/* parameterize api vs api.dev and replace string in json after importing */}
 // {/* 2. API swagger docs on separate Page called Docs with NavLink in header */}
