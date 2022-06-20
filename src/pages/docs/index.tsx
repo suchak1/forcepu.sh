4acb60d6add6b27fd6b58d5dfe19c8dd471ec430
Krish Suchak <suchak.krish@gmail.com>
2022-06-20 02:18:44 -0400

    spacing work complete

--- src/pages/docs/index.tsx
+++ src/pages/docs/index.tsx
@@ -67,136 +67,122 @@ const DocsPage = () => {
   // 4. Move Docs tab to the right? and remove signed in as User text
 
   return (
-    <div style={{ height: "100%" }}>
+    <>
       <Title>API Docs</Title>
       {/* add message when req fails that you need a valid api key */}
       {/* login and fetch account */}
       {/* hide api key component or indicate that you can login to get one */}
       {/* CHOOSE LATTER ^ */}
-      <div
-        style={{
-          display: "flex",
-          height: "100%",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
+      <Title level={2}>Auth</Title>
+      <div style={{ paddingBottom: "22px", marginTop: "-4px" }}>
+        <div>{"Use this key to authenticate your API requests."}</div>
         <div>
-          <Title level={2}>Auth</Title>
-          <div style={{ paddingBottom: "22px", marginTop: "-4px" }}>
-            <div>{"Use this key to authenticate your API requests."}</div>
-            <div>
-              <span>{"Header: "}</span>
-              <span style={{ fontFamily: "monospace" }}>{"X-API-Key"}</span>
-            </div>
-          </div>
-          <Input.Group style={{ paddingBottom: "26px" }}>
-            <span style={{ display: "flex" }}>
-              {/* use tooltip and/or message above explaining that this is needed to access endpoint */}
-              {/* and header is X-API-Key */}
-              <Tooltip
-                title="Use the button on the right to copy."
-                placement="bottom"
-              >
-                <APIKey
-                  // style={{
-                  //   userSelect: "none",
-                  //   "-webkit-user-select": "none",
-                  //   "user-select": "none",
-                  // }}
-                  style={{ userSelect: "none" }}
-                  addonBefore="API Key"
-                  defaultValue={
-                    loggedIn
-                      ? account?.api_key
-                      : "Log in to receive your API key."
-                  }
-                  readOnly
-                />
-              </Tooltip>
-              <Button
-                onClick={() => copyToClipboard(account?.api_key, "API Key")}
-                icon={<CopyOutlined />}
-              />
-              {/* handle copying to clipboard */}
-              {/* show success or info alert for a few sec at top when Copy button is pressed */}
-            </span>
-          </Input.Group>
-        </div>
-        <div>
-          <Title level={2}>API</Title>
-          <SwaggerUI
-            spec={swaggerSpec}
-            defaultModelsExpandDepth={0}
-            requestInterceptor={(req: Request) => {
-              console.log(req);
-              const { headers } = req;
-              if (!("X-API-Key" in headers)) {
-                notification.error({
-                  duration: 10,
-                  message: "Missing API Key",
-                  description: (
-                    <>
-                      <span>{"Paste your API key in the "}</span>
-                      <b>
-                        <span style={{ color: "#49cc90" }}>Authorize</span>
-                      </b>
-                      <span>{" modal."}</span>
-                    </>
-                  ),
-                });
-              } else if (headers["X-API-Key"] !== account?.api_key) {
-                notification.error({
-                  duration: 10,
-                  message: "Wrong API Key",
-                  description: "Copy your API key from the Auth section.",
-                });
-              }
-              req.url = "https://jsonplaceholder.typicode.com/todos/1";
-              return req;
-            }}
-            responseInterceptor={(res: Response) => {
-              console.log(res);
-              const errors = {
-                401: {
-                  message: "Unauthorized",
-                  description: "Check your API key.",
-                },
-                402: {
-                  message: "Payment Required",
-                  description: "You are not a beta subscriber.",
-                },
-                403: {
-                  message: "Forbidden",
-                  description: "Your quota has been reached.",
-                },
-              };
-              const { ok, status } = res;
-              if (ok) {
-                const { data } = JSON.parse(res.text);
-                const signal = data[data.length - 1].Signal;
-                notification.success({
-                  duration: 10,
-                  message: "Success",
-                  // Modify description to say "New signal: BUY or SELL" lime or red monospace
-                  description: "Your request succeeded.",
-                });
-              } else {
-                notification.error({
-                  duration: 10,
-                  message:
-                    status in errors ? errors[status].message : "Failure",
-                  description:
-                    status in errors
-                      ? errors[status].description
-                      : "Your request failed. Check the error message.",
-                });
-              }
-            }}
-          />
+          <span>{"Header: "}</span>
+          <span style={{ fontFamily: "monospace" }}>{"X-API-Key"}</span>
         </div>
       </div>
-    </div>
+      <Input.Group style={{ paddingBottom: "26px" }}>
+        <span style={{ display: "flex" }}>
+          {/* use tooltip and/or message above explaining that this is needed to access endpoint */}
+          {/* and header is X-API-Key */}
+          <Tooltip
+            title="Use the button on the right to copy."
+            placement="bottom"
+          >
+            <APIKey
+              // style={{
+              //   userSelect: "none",
+              //   "-webkit-user-select": "none",
+              //   "user-select": "none",
+              // }}
+              style={{ userSelect: "none" }}
+              addonBefore="API Key"
+              defaultValue={
+                loggedIn ? account?.api_key : "Log in to receive your API key."
+              }
+              readOnly
+            />
+          </Tooltip>
+          <Button
+            onClick={() => copyToClipboard(account?.api_key, "API Key")}
+            icon={<CopyOutlined />}
+          />
+          {/* handle copying to clipboard */}
+          {/* show success or info alert for a few sec at top when Copy button is pressed */}
+        </span>
+      </Input.Group>
+      <Title level={2}>API</Title>
+
+      <SwaggerUI
+        spec={swaggerSpec}
+        // can make this 0 to collapse Schema
+        defaultModelsExpandDepth={1}
+        requestInterceptor={(req: Request) => {
+          console.log(req);
+          const { headers } = req;
+          if (!("X-API-Key" in headers)) {
+            notification.error({
+              duration: 10,
+              message: "Missing API Key",
+              description: (
+                <>
+                  <span>{"Paste your API key in the "}</span>
+                  <b>
+                    <span style={{ color: "#49cc90" }}>Authorize</span>
+                  </b>
+                  <span>{" modal."}</span>
+                </>
+              ),
+            });
+          } else if (headers["X-API-Key"] !== account?.api_key) {
+            notification.error({
+              duration: 10,
+              message: "Wrong API Key",
+              description: "Copy your API key from the Auth section.",
+            });
+          }
+          req.url = "https://jsonplaceholder.typicode.com/todos/1";
+          return req;
+        }}
+        responseInterceptor={(res: Response) => {
+          console.log(res);
+          const errors = {
+            401: {
+              message: "Unauthorized",
+              description: "Check your API key.",
+            },
+            402: {
+              message: "Payment Required",
+              description: "You are not a beta subscriber.",
+            },
+            403: {
+              message: "Forbidden",
+              description: "Your quota has been reached.",
+            },
+          };
+          const { ok, status } = res;
+          if (ok) {
+            const { data } = JSON.parse(res.text);
+            const signal = data[data.length - 1].Signal;
+            notification.success({
+              duration: 10,
+              message: "Success",
+              // Modify description to say "New signal: BUY or SELL" lime or red monospace
+              description: "Your request succeeded.",
+            });
+          } else {
+            notification.error({
+              duration: 10,
+              message: status in errors ? errors[status].message : "Failure",
+              description:
+                status in errors
+                  ? errors[status].description
+                  : "Your request failed. Check the error message.",
+            });
+          }
+        }}
+      />
+    </>
   );
 };
 
