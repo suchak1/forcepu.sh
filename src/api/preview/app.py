210667252dbf3efc1ec687f3bbf6f027b597a020
Krish Suchak <suchak.krish@gmail.com>
2022-09-18 14:59:31 -0400

    add model api to preview

--- src/api/preview/app.py
+++ src/api/preview/app.py
@@ -1,4 +1,5 @@
 import os
+import json
 import boto3
 
 s3 = boto3.client('s3')
@@ -12,3 +13,23 @@ def get_preview(*_):
         "body": obj['Body'].read(),
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
+
+
+def get_model(*_):
+    obj = s3.get_object(
+        Bucket=os.environ['S3_BUCKET'], Key='models/latest/metadata.json')
+    metadata = json.loads(obj['Body'].read())
+    metadata['num_features'] = len(metadata['features'])
+    allowed_fields = ['created', 'start', 'end', 'num_features', 'accuracy']
+    metadata = {key: metadata[key] for key in allowed_fields}
+    return {
+        "statusCode": 200,
+        "body": json.dumps(metadata),
+        "headers": {"Access-Control-Allow-Origin": "*"}
+    }
+
+
+# bytes_buffer = io.BytesIO()
+# s3.download_fileobj(Bucket=bucket_name,
+#                     Key='models/latest/metadata.json', Fileobj=bytes_buffer)
+# value = json.loads(bytes_buffer.getvalue())
