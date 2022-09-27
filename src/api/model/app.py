59ad794d66e5fcce6ec037d2548803f1bd61be60
Krish Suchak <suchak.krish@gmail.com>
2022-09-27 07:51:24 -0400

    add np encoder

--- src/api/model/app.py
+++ src/api/model/app.py
@@ -2,7 +2,7 @@ import os
 import json
 import boto3
 import pickle
-# import numpy
+import numpy as np
 s3 = boto3.client('s3')
 
 
@@ -37,16 +37,44 @@ def get_visualization(event, _):
 
     data = {
         label:
-        (float if label == 'radius' else list)(
+        # (float if label == 'radius' else list)(
             pickle.loads(
                 s3.get_object(
                     Bucket=os.environ['S3_BUCKET'],
                     Key=f'models/latest/{dims}/{label}.pkl')['Body'].read()
-            )
-        ) for label in data_labels
+                # )
+            ) for label in data_labels
     }
     return {
         "statusCode": 200,
-        "body": json.dumps(data),
+        "body": json.dumps(data, cls=NumpyEncoder),
         "headers": {"Access-Control-Allow-Origin": "*"}
     }
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        elif isinstance(obj, (np.void)):
+            return None
+
+        return json.JSONEncoder.default(self, obj)
