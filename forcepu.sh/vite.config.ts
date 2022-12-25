85ae9887c11e474e43a25cffe475eff07e987fb3
Krish Suchak <suchak.krish@gmail.com>
2022-12-24 19:09:08 -0500

    Revert "filesystem routing experiment"
    This reverts commit 135a63dea7418dc6bfcc2f15860d058e5ee80184.

--- forcepu.sh/vite.config.ts
+++ forcepu.sh/vite.config.ts
@@ -1,16 +1,7 @@
-import * as path from 'path'
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-// import pages from 'vite-plugin-react-pages'
-import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    preact(),
-    // pages({
-    //   pagesDir: path.join(__dirname, 'pages'),
-    // })
-    ssr(),
-  ],
+  plugins: [preact()],
 })
