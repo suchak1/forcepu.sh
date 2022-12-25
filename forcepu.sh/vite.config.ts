135a63dea7418dc6bfcc2f15860d058e5ee80184
Krish Suchak <suchak.krish@gmail.com>
2022-12-24 19:07:36 -0500

    filesystem routing experiment

--- forcepu.sh/vite.config.ts
+++ forcepu.sh/vite.config.ts
@@ -1,7 +1,16 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+// import pages from 'vite-plugin-react-pages'
+import ssr from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    // pages({
+    //   pagesDir: path.join(__dirname, 'pages'),
+    // })
+    ssr(),
+  ],
 })
