cfc5e3e0b39a1e5bf57b4369070d8d4853e04855
Krish Suchak <suchak.krish@gmail.com>
2023-01-02 02:02:29 -0500

    quiet

--- util/install_sam.sh
+++ util/install_sam.sh
@@ -3,7 +3,7 @@
 set -e
 
 curl -LO https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
-unzip -o aws-sam-cli-linux-x86_64.zip -d sam-installation
+unzip -o -q aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
 rm -rf sam-installation aws-sam-cli-linux-x86_64.zip
 
