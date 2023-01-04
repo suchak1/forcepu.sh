57e1868fa19a9215085b9c49848dc8f5d7862d4b
Krish Suchak <suchak.krish@gmail.com>
2023-01-04 08:24:16 -0500

    starting backend

--- src/api/subscribe/app.py
+++ src/api/subscribe/app.py
@@ -1,41 +1,55 @@
 import stripe
 
 
-def post_subscribe(*_):
-    stripe.api_key = "sk_test_abc"
-    # see if customer exists
-    # else create
-    stripe.Customer.create(
-        email="{{CUSTOMER_EMAIL}}",
-        name="{{CUSTOMER_NAME}}",
-        shipping={
-            "address": {
-                "city": "Brothers",
-                "country": "US",
-                "line1": "27 Fredrick Ave",
-                "postal_code": "97712",
-                "state": "CA",
-            },
-            "name": "{{CUSTOMER_NAME}}",
-        },
-        address={
-            "city": "Brothers",
-            "country": "US",
-            "line1": "27 Fredrick Ave",
-            "postal_code": "97712",
-            "state": "CA",
-        },
-    )
-    # save customerid to db?
-    # new_customer var if not in db
+def post_checkout(*_):
+    # use auto tax
+    # use trial period
+    # mode: subscription
+    pass
 
-    # if not new customer, update customer - address?
+def post_billing(*_):
+    # create customer portal session
+    # (where customer can cancel, change payment method, see payment history, etc)
+    # find a way of embedding html directly into right side or modal
+    # vs just redirect to stripe hosted (?) customer portal page
 
-    # process payment
-    # if error and new_customer, delete customer?
+# def post_subscribe(*_):
+    # handle webhooks
 
-    return {
-        "statusCode": 200,
-        "body": 'necessary payment stuff',
-        "headers": {"Access-Control-Allow-Origin": "*"}
-    }
+#     stripe.api_key = "sk_test_abc"
+#     # see if customer exists
+#     # else create
+#     stripe.Customer.create(
+#         email="{{CUSTOMER_EMAIL}}",
+#         name="{{CUSTOMER_NAME}}",
+#         shipping={
+#             "address": {
+#                 "city": "Brothers",
+#                 "country": "US",
+#                 "line1": "27 Fredrick Ave",
+#                 "postal_code": "97712",
+#                 "state": "CA",
+#             },
+#             "name": "{{CUSTOMER_NAME}}",
+#         },
+#         address={
+#             "city": "Brothers",
+#             "country": "US",
+#             "line1": "27 Fredrick Ave",
+#             "postal_code": "97712",
+#             "state": "CA",
+#         },
+#     )
+#     # save customerid to db?
+#     # new_customer var if not in db
+
+#     # if not new customer, update customer - address?
+
+#     # process payment
+#     # if error and new_customer, delete customer?
+
+#     return {
+#         "statusCode": 200,
+#         "body": 'necessary payment stuff',
+#         "headers": {"Access-Control-Allow-Origin": "*"}
+#     }
