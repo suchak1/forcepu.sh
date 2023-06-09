08ad3785a9bd0439442f6664d5a500ea1e625199
Krish Suchak <suchak.krish@gmail.com>
2023-06-09 19:40:56 -0400

    test that links are in header

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,14 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:8000')
+    cy.get('.ant-layout-header').should(el => {
+      const element = el[0];
+      expect(element.innerText).to.include('FORCEPU.SH');
+      expect(element.innerText).to.include('Docs');
+      expect(element.innerText).to.include('Algorithm');
+      expect(element.innerText).to.include('Subscription');
+      expect(element.innerText).to.include('Alerts');
+      expect(element.innerText).to.include('Contact');
+    })
   })
 })
