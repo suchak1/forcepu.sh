b132b8a37510b35dcd8502730354f3b67f540a01
Krish Suchak <suchak.krish@gmail.com>
2023-06-09 19:56:24 -0400

    test that links are in footer

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+// Code coverage
+// https://docs.cypress.io/guides/tooling/code-coverage#E2E-and-unit-code-coverage
+
+describe('Navigation', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:8000')
+  })
+  it('Header', () => {
     cy.get('.ant-layout-header').should(el => {
       const element = el[0];
       expect(element.innerText).to.include('FORCEPU.SH');
@@ -9,6 +14,14 @@ describe('template spec', () => {
       expect(element.innerText).to.include('Subscription');
       expect(element.innerText).to.include('Alerts');
       expect(element.innerText).to.include('Contact');
+    })
+  })
+  it('Footer', () => {
+    cy.get('.ant-layout-footer').should(el => {
+      const element = el[0];
+      expect(element.innerText).to.include('Terms of Service');
+      expect(element.innerText).to.include('Financial Disclaimer');
+      expect(element.innerText).to.include('Privacy');
     })
   })
 })
