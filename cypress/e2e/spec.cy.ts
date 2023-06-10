04ce26ef5e68d118a1b632c671d6960fd20d127b
Krish Suchak <suchak.krish@gmail.com>
2023-06-10 16:41:50 -0400

    change port

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,7 +3,7 @@
 
 describe('Navigation', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5173')
+    cy.visit('http://localhost:8000')
   })
   it('Header', () => {
     const selector = '.ant-layout-header';
