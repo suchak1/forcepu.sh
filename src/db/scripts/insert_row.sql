3f916617cb5323e89ee81fd2d40cc96c818deb36
Krish Suchak <suchak.krish@gmail.com>
2023-06-09 21:06:24 -0400

    cypress gh action

--- src/db/scripts/insert_row.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (email, permissions) VALUES('ks', '{"isAdmin": false}');
--- src/db/scripts/list_engine_versions.sh
+++
@@ -1,2 +0,0 @@
-aws rds describe-db-engine-versions | jq '.DBEngineVersions[] | select((.Engine == "aurora-postgresql") and (.SupportedEngineModes[] | contains("serverless"))) | .EngineVersion'
-# aws rds describe-db-engine-versions --engine aurora-postgresql --query "DBEngineVersions[].EngineVersion"
