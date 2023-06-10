e5cb693cfb834bd4ebe6e320ec1d8720864533bb
Krish Suchak <suchak.krish@gmail.com>
2023-06-09 23:32:23 -0400

    add proxy bypass

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,30 @@ export default {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+        console.log(launchOptions.args); // print all current args
+
+        launchOptions.args = launchOptions.args.map((arg) => {
+          if (arg.startsWith("--proxy-bypass-list")) {
+            return "--proxy-bypass-list=<-loopback>,ws://*localhost*:8000";
+          }
+
+          return arg;
+        });
+
+        if (browser.family === "chromium" && browser.name !== "electron") {
+          // auto open devtools
+          launchOptions.args.push("--auto-open-devtools-for-tabs");
+        }
+
+        // whatever you return here becomes the launchOptions
+
+        console.log("final args", launchOptions.args); // print all current args
+
+        return launchOptions;
+      });
     },
     testIsolation: false,
   },
