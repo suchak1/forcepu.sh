3a170c865e1db4bc0a56e0bc5c599e1d77b80b0f
Krish Suchak <suchak.krish@gmail.com>
2023-06-09 20:12:37 -0400

    test that docs link goes to docs page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -8,13 +8,24 @@ describe('Navigation', () => {
   it('Header', () => {
     cy.get('.ant-layout-header').should(el => {
       const element = el[0];
+      // Test that links exist
       expect(element.innerText).to.include('FORCEPU.SH');
       expect(element.innerText).to.include('Docs');
       expect(element.innerText).to.include('Algorithm');
       expect(element.innerText).to.include('Subscription');
       expect(element.innerText).to.include('Alerts');
       expect(element.innerText).to.include('Contact');
+
+
     })
+    cy.get('.ant-layout-header').find('a').contains('Docs').first().click();
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/docs')
+    })
+    // cy.get('a').contains('Docs').first().click();
+    // console.log(cy.url())
+    // cy.get('a:contains("Docs")').first().click()
+    // console.log(cy.url())
   })
   it('Footer', () => {
     cy.get('.ant-layout-footer').should(el => {
