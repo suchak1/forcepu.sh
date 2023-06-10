ac120969af62ad062f42e6c9cc06d7852068b800
Krish Suchak <suchak.krish@gmail.com>
2023-06-10 16:55:23 -0400

    start at docs page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,7 +3,7 @@
 
 describe('Navigation', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8000')
+    cy.visit('http://localhost:8000/docs')
   })
   it('Header', () => {
     const selector = '.ant-layout-header';
