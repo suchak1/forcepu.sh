e3e16391b8ac7b4e55416a415f7b786aaa2ae753
Krish Suchak <suchak.krish@gmail.com>
2023-06-10 20:17:48 -0400

    use vite-cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,33 +1,47 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import vitePreprocessor from 'cypress-vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default {
   video: Boolean(process.env.CI),
   e2e: {
-    // setupNodeEvents(on, config) {
-    //   // implement node event listeners here
-    //   // on("before:browser:launch", (browser = {}, launchOptions) => {
-    //   //   // `args` is an array of all the arguments that will
-    //   //   // be passed to browsers when it launches
-    //   //   console.log(launchOptions.args); // print all current args
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      on(
+        'file:preprocessor',
+        vitePreprocessor({
+          configFile: path.resolve(__dirname, './vite.config.ts'),
+          mode: 'development',
+        }),
+      )
+      //   // on("before:browser:launch", (browser = {}, launchOptions) => {
+      //   //   // `args` is an array of all the arguments that will
+      //   //   // be passed to browsers when it launches
+      //   //   console.log(launchOptions.args); // print all current args
 
-    //   //   launchOptions.args = launchOptions.args.map((arg) => {
-    //   //     if (arg.startsWith("--proxy-bypass-list")) {
-    //   //       return "--proxy-bypass-list=<-loopback>,ws://*localhost*:8000";
-    //   //     }
+      //   //   launchOptions.args = launchOptions.args.map((arg) => {
+      //   //     if (arg.startsWith("--proxy-bypass-list")) {
+      //   //       return "--proxy-bypass-list=<-loopback>,ws://*localhost*:8000";
+      //   //     }
 
-    //   //     return arg;
-    //   //   });
+      //   //     return arg;
+      //   //   });
 
-    //   //   if (browser.family === "chromium" && browser.name !== "electron") {
-    //   //     // auto open devtools
-    //   //     launchOptions.args.push("--auto-open-devtools-for-tabs");
-    //   //   }
+      //   //   if (browser.family === "chromium" && browser.name !== "electron") {
+      //   //     // auto open devtools
+      //   //     launchOptions.args.push("--auto-open-devtools-for-tabs");
+      //   //   }
 
-    //   //   // whatever you return here becomes the launchOptions
+      //   //   // whatever you return here becomes the launchOptions
 
-    //   //   console.log("final args", launchOptions.args); // print all current args
+      //   //   console.log("final args", launchOptions.args); // print all current args
 
-    //   //   return launchOptions;
-    //   // });
-    // },
+      //   //   return launchOptions;
+      //   // });
+    },
     // // testIsolation: false,
   },
 };
