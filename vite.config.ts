e8e913f4af1659b74e7dea0ae772e943256d3b24
Krish Suchak <suchak.krish@gmail.com>
2023-06-11 17:06:17 -0400

    add coverage report and check

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { getThemeVariables } from 'antd/dist/theme';
+import istanbul from 'vite-plugin-istanbul';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,37 +13,17 @@ export default defineConfig({
     rollupOptions: {
       output: {
         assetFileNames: `assets/[name].[ext]`
-        // "assets/[name]-[hash][extname]"
       }
     }
   },
-  // define: {
-  // ['global.']: 'window.',
-  // global: 'globalThis',
 
-  // globalThis may be better
-  // https://blog.logrocket.com/what-is-globalthis-why-use-it/
-  // },
-  plugins: [react()],
+  plugins: [react(), istanbul()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      // react: 'preact/compat',
       './runtimeConfig': './runtimeConfig.browser',
     },
   },
-  // css: {
-  //   preprocessorOptions: {
-  //     less: {
-  //       modifyVars: { 'borderRadiusLG': 2 },
-  //       // modifyVars: getThemeVariables({
-  //       //   dark: true,
-  //       //   // compact: true,
-  //       // }),
-  //       javascriptEnabled: true,
-  //     },
-  //   },
-  // },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
@@ -53,15 +33,3 @@ export default defineConfig({
     },
   },
 })
-
-// AWS Polyfill issues
-// https://github.com/aws-amplify/amplify-js/issues/9639
-// add this to index.html
-// <script>
-//     var global = global || window;
-//     var Buffer = Buffer || [];
-//     var process = process || {
-//       env: { DEBUG: undefined },
-//       version: []
-//     };
-// </script>
