0171989e7c525f67f2dc88a7450a13d577886e14
Krish Suchak <suchak.krish@gmail.com>
2023-07-20 03:22:05 -0400

    fix test

--- src/api/test/preview/test_preview.py
+++ src/api/test/preview/test_preview.py
@@ -8,6 +8,13 @@ def test_get_preview():
     res = get_preview()
     assert res['statusCode'] == 200
     data = json.loads(res['body'])
-    assert set(['Date', 'Id', 'Weight', 'Reps', 'Exercise',
-               'Volume', '1RM']).issubset(data[0].keys())
+
+    assert set(['Bal', 'Name', 'Time']).issubset(data['BTC']['data'][0].keys())
+    assert set(['Bal', 'Name', 'Time']).issubset(data['USD']['data'][0].keys())
+
+    assert set(['Total Return [%]', 'Max Drawdown [%]', 'Win Rate [%]', 'Profit Factor', 'Total Fees Paid', 'Profitable Time [%]']).issubset(
+        {datum['metric'] for datum in data['BTC']['stats']})
+    assert set(['Total Return [%]', 'Max Drawdown [%]', 'Win Rate [%]', 'Profit Factor', 'Sharpe Ratio', 'Sortino Ratio']).issubset(
+        {datum['metric'] for datum in data['USD']['stats']})
+
     assert res['headers']['Access-Control-Allow-Origin'] == '*'
