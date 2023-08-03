dc72c485ee8d646c440a4add3d07537da5b03bda
Krish Suchak <suchak.krish@gmail.com>
2023-08-03 14:57:50 -0400

    improve alerts user model tests

--- src/api/test/shared/python/test_models.py
+++ src/api/test/shared/python/test_models.py
@@ -17,12 +17,20 @@ def test_get_default_access_queue():
     assert get_default_access_queue() == [past_date] * 5
 
 
+def verify_alerts(alerts):
+    assert type(alerts.email) == bool
+    assert not alerts.email
+    assert type(alerts.sms) == bool
+    assert not alerts.sms
+    assert type(alerts.webhook) == str
+    assert not alerts.webhook
+    assert type(alerts.last_sent) == str
+    assert alerts.last_sent == UTCDateTimeAttribute().serialize(past_date)
+
+
 class TestAlerts():
     alerts = Alerts()
-    assert type(alerts.email) == bool
-    assert type(alerts.sms) == bool
-    assert type(alerts.webhook) == str
-    assert type(alerts.last_sent) == str
+    verify_alerts(alerts)
 
 
 class TestPermissions():
@@ -63,21 +71,22 @@ class TestSubscribedIndex():
 
 
 class TestUserModel():
-    user_model = UserModel('test_user')
-    assert type(user_model.email) == str
-    assert user_model.email == 'test_user'
-    assert type(user_model.api_key) == str
-    assert len(user_model.api_key) == 86
-    assert type(user_model.alerts) == Alerts
-    assert type(user_model.permissions) == Permissions
-    assert type(user_model.in_beta) == int
-    assert user_model.in_beta == 0
-    assert type(user_model.subscribed) == int
-    assert user_model.subscribed == 0
-    assert type(user_model.stripe) == Stripe
-    assert type(user_model.access_queue) == list
-    assert user_model.access_queue == [past_date] * 5
-    assert type(user_model.api_key_index) == APIKeyIndex
-    assert type(user_model.customer_id_index) == CustomerIdIndex
-    assert type(user_model.in_beta_index) == InBetaIndex
-    assert type(user_model.subscribed_index) == SubscribedIndex
+    user = UserModel('test_user')
+    assert type(user.email) == str
+    assert user.email == 'test_user'
+    assert type(user.api_key) == str
+    assert len(user.api_key) == 86
+    assert type(user.alerts) == Alerts
+    verify_alerts(user.alerts)
+    assert type(user.permissions) == Permissions
+    assert type(user.in_beta) == int
+    assert user.in_beta == 0
+    assert type(user.subscribed) == int
+    assert user.subscribed == 0
+    assert type(user.stripe) == Stripe
+    assert type(user.access_queue) == list
+    assert user.access_queue == [past_date] * 5
+    assert type(user.api_key_index) == APIKeyIndex
+    assert type(user.customer_id_index) == CustomerIdIndex
+    assert type(user.in_beta_index) == InBetaIndex
+    assert type(user.subscribed_index) == SubscribedIndex
