fc4d40fe81dd8652dbd2ea67084b38d57c425e41
Krish Suchak <suchak.krish@gmail.com>
2023-08-12 01:00:53 -0400

    local api

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,14 @@ import istanbul from 'vite-plugin-istanbul';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8000
+    port: 8000,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   },
   build: {
     target: 'esnext',
