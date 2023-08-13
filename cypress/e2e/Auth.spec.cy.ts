140c64de1b128dc7c6db0791a255cf5948ebd1d6
Krish Suchak <suchak.krish@gmail.com>
2023-08-13 06:00:09 -0400

    fix api test

--- cypress/e2e/Auth.spec.cy.ts
+++ cypress/e2e/Auth.spec.cy.ts
@@ -9,5 +9,13 @@ describe('Auth', () => {
         cy.get(selector).find('input[name="username"]').last().type(`${Cypress.env('SIGNAL_EMAIL')}@dev.forcepu.sh`);
         cy.get(selector).find('input[name="password"]').last().type(Cypress.env('EMAIL_PASS'));
         cy.get(selector).find('button[type="submit"]').contains('Sign in').click();
+        cy.intercept('GET', 'https://api.dev.forcepu.sh/account').as('getAccount');
+        cy.wait('@getAccount').then(({request, ...rest}) => {
+            const auth = request.headers['authorization'];
+            // console.log('Auth', Authorization);
+            console.log('req', request);
+            console.log('headers', request.headers);
+            cy.request({method: 'DELETE', url: 'https://api.dev.forcepu.sh/account', headers: {Authorization: auth}});
+        });        
     })
   })
