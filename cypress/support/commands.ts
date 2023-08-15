d8dd3b507175835c2eb519e49200beebb0683187
Krish Suchak <suchak.krish@gmail.com>
2023-08-15 03:30:13 -0400

    hide secret

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -16,7 +16,7 @@ Cypress.Commands.add('login', () => {
     cy.get(selector).find('button').contains('Get started').first().click();
     selector = '.ant-modal-body';
     cy.get(selector).find('input[name="username"]').last().type(`${Cypress.env('SIGNAL_EMAIL')}@dev.forcepu.sh`);
-    cy.get(selector).find('input[name="password"]').last().type(Cypress.env('EMAIL_PASS'));
+    cy.get(selector).find('input[name="password"]').last().type(Cypress.env('EMAIL_PASS'), {log: false});
     cy.get(selector).find('button[type="submit"]').contains('Sign in').click();
 });
 //
