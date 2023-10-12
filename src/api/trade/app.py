a2e5e35409ab9858ab91f657f81c29c062c10d4d
Krish Suchak <suchak.krish@gmail.com>
2023-10-12 02:57:21 -0400

    simplify get_expirs func

--- src/api/trade/app.py
+++ src/api/trade/app.py
@@ -143,13 +143,13 @@ def get_expirations(expirations, num=2):
     today = datetime.now()
     week = set([datetime.strftime(day, '%Y-%m-%d') for day in get_week(today)])
     exp_candidates = [None]
-    for exp in expirations:
+    last_day_in_week = 0
+    for idx, exp in enumerate(expirations):
         if exp in week:
-            exp_candidates[0] = exp
+            last_day_in_week = idx
         else:
-            exp_candidates.append(exp)
-            if len(exp_candidates) >= num:
-                break
+            break
+    exp_candidates = expirations[last_day_in_week:last_day_in_week + num]
     return exp_candidates
 
 def get_contracts(symbol, expiration, num=2):
